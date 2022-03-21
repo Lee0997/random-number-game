fa0f566f4532e179fddea4377fd48668314a6c42
Lee <leenock2012@yahoo.co.uk>
2022-03-21 15:33:58 +0000

    Added the basics

--- Runner.java
+++ Runner.java
@@ -1,10 +1,15 @@
 package com.qa;
 
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+        int attempt = 1;
+        int guess = 0;
+        int randomNumber = (int)(Math.random() * 99 + 1);
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Welcome to the random number generator game");
+    }
 }
