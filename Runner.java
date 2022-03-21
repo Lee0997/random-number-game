449da59106c6e8266efe2efc21a15cc6dcb6965f
Lee <leenock2012@yahoo.co.uk>
2022-03-21 15:47:27 +0000

    Added the foundation of the game

--- Runner.java
+++ Runner.java
@@ -6,10 +6,19 @@ public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int attempt = 1;
-        int guess = 0;
-        int randomNumber = (int)(Math.random() * 99 + 1);
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Welcome to the random number generator game");
-    }
+		Scanner userInput = new Scanner(System.in);
+		int randomNumber = (int) (Math.random() * 99 + 1);
+		int guess;
+		
+		System.out.println("Welcome to the random number generator game");
+		System.out.print("Pick a number: ");
+		guess = userInput.nextInt();
+		
+		if (guess == randomNumber) {
+			System.out.println("Well done, the number was: " + randomNumber + ", you got the right number!");
+		} else if (guess != randomNumber) {
+			System.out.println("Sorry, the number was: " + randomNumber +  ", looks like you were incorrect! Better luck next time");
+		}
+		userInput.close();
+	}
 }
